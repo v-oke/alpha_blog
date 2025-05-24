eaac02da68fcce8ec2e9ff264ace1115e273f299
Kelvin Gicheha <kwachira@outlook.com>
2025-05-24 12:04:51 +0300

    Prepare for deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit,:destroy,:update,:show]
 
   def index
     @articles = Article.all
@@ -20,11 +21,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
@@ -39,13 +38,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully Deleted"
     redirect_to articles_path
   end
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
