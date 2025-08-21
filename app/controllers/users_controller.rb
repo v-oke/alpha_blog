bb346cb6443b0e109028dc5fb835e691ef0b59dc
Kelvin Gicheha <kwachira@outlook.com>
2025-08-21 16:21:23 +0300

    Sign up users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      Rails.logger.debug "Validation errors: #{@user.errors.full_messages.inspect}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,8 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   resources :articles
+
+  get "signup", to: "users#new"
+
+  resources :users, except: [:new]
 end
