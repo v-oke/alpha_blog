2202cbf21a9fbe9ba016c4029f75a0a6e41927c6
Kelvin Gicheha <kwachira@outlook.com>
2025-09-01 21:15:14 +0300

    add show page for user's articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user)
+    src="https://avatar.iran.liara.run/public"
+    alt_name=user.username
+    cls="rounded-circle img-fluid"
+    stl="width: 100px; height: 100px; object-fit: cover;"
+    image_tag(src, alt: alt_name, class: cls, style: stl)
+  end
 end
