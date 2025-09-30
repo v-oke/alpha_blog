3b4d03bf7acd1565e379d3b149da9c8d2d4af117
Kelvin Gicheha <kwachira@outlook.com>
2025-09-30 14:41:50 +0300

    add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [ :show, :index ]
-  before_action :set_category, only: [ :show ]
+  before_action :set_category, only: [ :show, :edit, :update ]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 4)
@@ -25,6 +25,19 @@ class CategoriesController < ApplicationController
 
   def show
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully Updated"
+      redirect_to category_path(@category)
+    else
+      Rails.logger.debug "Validation errors: #{@category.errors.full_messages.inspect}"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
